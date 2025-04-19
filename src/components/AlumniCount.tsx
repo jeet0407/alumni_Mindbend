6a3385bca663e8d34fb6d12cf9260bab74d5f1a9
jeet0407 <jeetandel0404@gmail.com>
2025-04-19 14:45:57 +0530

    removed select year

--- src/components/AlumniCount.tsx
+++ src/components/AlumniCount.tsx
@@ -133,23 +133,6 @@ function AlumniCount() {
               </div>
             </div>
           </div>
-        <div className="mt-4">
-          <label htmlFor="year-select" className="block text-sm font-medium text-gray-700">
-            Select Year
-          </label>
-          <select
-            id="year-select"
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
-            value={selectedYear}
-            onChange={handleYearChange}
-          >
-            {Object.keys(alumniData).map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
-        </div>
     </div>
   )
 }
